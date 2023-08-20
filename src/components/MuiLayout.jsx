38200c64468dc92b4669655faa2f4263accebf04
Fredys Fernandez <juniorfrey2990@gmail.com>
2023-08-19 20:05:02 -0500

    Commit Stack

--- src/components/MuiLayout.jsx
+++ src/components/MuiLayout.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import {Box} from '@mui/material'
+import {Box, Stack, Divider} from '@mui/material'
 
 export const MuiLayout = () => {
   return (
-    <>
-    <Box sx={{
-        backgroundColor:'primary.main',
-        color:'white',
-        height:'100px',
-        width:'100px',
-        padding:'16px',
-        '&:hover':{
-            backgroundColor:'primary.light',
-        }
-    }}>
-        Codevolution
-    </Box>
-    <Box display='flex' height='100px' width='100px' bgcolor='success.light' p={2}>
+    <Stack sx={{border:'1px solid  '}} direction='row' spacing={2} 
+        divider={<Divider orientation='vertical' flexItem color='success' />}>
+        <Box sx={{
+            backgroundColor:'primary.main',
+            color:'white',
+            height:'100px',
+            width:'100px',
+            padding:'16px',
+            '&:hover':{
+                backgroundColor:'primary.light',
+            }
+        }}>
+            Codevolution
+        </Box>
+        <Box display='flex' height='100px' width='100px' bgcolor='success.light' p={2}>
 
-    </Box>
-    </>
+        </Box>
+    </Stack>
   )
 }
